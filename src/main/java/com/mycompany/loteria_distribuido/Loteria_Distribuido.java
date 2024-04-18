295ad19ab3c51db0c61cea626678c7a4712ca401
Carlos <carlos.cantrax303@gmail.com>
2024-04-18 17:41:20 -0500

    Primer Commit

---
+++ src/main/java/com/mycompany/loteria_distribuido/Loteria_Distribuido.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.loteria_distribuido;
+
+/**
+ *
+ * @author labinfo02
+ */
+public class Loteria_Distribuido {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
