b6c661f6439cb3eadd213dde2d02bd212885a055
Skar13F <of5162@gmail.com>
2024-04-19 16:40:16 -0600

    Clases base

--- src/main/java/com/mycompany/loteria_distribuido/Loteria_Distribuido.java
+++ src/main/java/com/mycompany/loteria_distribuido/Loteria_Distribuido.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.loteria_distribuido;
 
-/**
- *
- * @author labinfo02
- */
 public class Loteria_Distribuido {
 
     public static void main(String[] args) {
