ae19530f05148c40db644cbf6f77fe96d46093eb
Skar13F <of5162@gmail.com>
2024-04-19 16:42:43 -0600

    Primer commit

---
+++ src/main/java/model/Carta.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author labinfo04
+ */
+public class Carta {
+    
+    private int idCarta;
+
+    public int getIdCarta() {
+        return idCarta;
+    }
+
+    public void setIdCarta(int idCarta) {
+        this.idCarta = idCarta;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRutaCarta() {
+        return rutaCarta;
+    }
+
+    public void setRutaCarta(String rutaCarta) {
+        this.rutaCarta = rutaCarta;
+    }
+    private String nombre;
+    private String rutaCarta;
+    
+    
+}
---
+++ src/main/java/model/Jugador.java
@@ -0,0 +1,25 @@
+
+package model;
+
+public class Jugador {
+    private String nombre;
+    private Carton carton;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Carton getCarton() {
+        return carton;
+    }
+
+    public void setCarton(Carton carton) {
+        this.carton = carton;
+    }
+    
+    
+}
