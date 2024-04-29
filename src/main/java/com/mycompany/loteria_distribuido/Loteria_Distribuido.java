77463958cf1ac5b349e0b1a911082e1558b72321
Skar13F <of5162@gmail.com>
2024-04-29 12:36:56 -0600

    carga  de imagenes

--- src/main/java/com/mycompany/loteria_distribuido/Loteria_Distribuido.java
+++
@@ -1,9 +0,0 @@
-
-package com.mycompany.loteria_distribuido;
-
-public class Loteria_Distribuido {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
